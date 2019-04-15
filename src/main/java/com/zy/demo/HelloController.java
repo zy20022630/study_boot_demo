876b3506278f4bb3a36a3c2c859b00b55747db8b
zy20022630 <103335460@qq.com>
2019-04-16 04:31:47 +0800

    V1.0

---
+++ src/main/java/com/zy/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.zy.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/springboot")
+    public String hello(){
+        return "Hello SpringBoot!";
+    }
+}
