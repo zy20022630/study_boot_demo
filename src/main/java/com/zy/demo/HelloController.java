dfaa8a9edc10dd7d4ef966f5c330df26e4ab0ca6
zy20022630 <103335460@qq.com>
2019-04-16 05:16:04 +0800

    v1.4

--- src/main/java/com/zy/demo/HelloController.java
+++ src/main/java/com/zy/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/springboot")
     public String hello(){
-        return "Hello SpringBoot! v1.3";
+        return "Hello SpringBoot! v1.4";
     }
 }
